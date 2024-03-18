6268e82580dc6c9603d06a70b7df20cc53f210f9
lin <lin.04290619@gmail.com>
2024-03-18 22:22:44 +0800

    shell 重定向

--- MY_SHELL/mysh.h
+++ MY_SHELL/mysh.h
@@ -6,9 +6,26 @@
 #include <sys/types.h>
 #include <string.h> 
 #include <fcntl.h>
+#include <ctype.h>
+#include <dirent.h>
 
-int mysh_cd(char **args);
-int mysh_help(char **args);
-int mysh_exit(char **args);
+#define MAX_CMD 50
+#define BUFFSIZE 255
+#define MAX_CMD_LEN 100
+#define MYSH_TOK_DELIM " \t\r\n"
+
+char * mysh_read_line();
 char ** mysh_split_line();
-int mysh_execute(char **args);
+int mysh_execute(char **); 
+int mysh_launch(char **);
+int mysh_builtin_nums();
+int mysh_cd(char **);
+int mysh_help(char **);
+int mysh_exit(char **);
+int mysh_history(char **);
+int cmd_OutPut(char *);
+int cmd_InPut(char *);
+int cmd_ReOutPut(char *);
+int cmd_Pipe(char *);
+int cmd_InBackground(char *buf);
+
