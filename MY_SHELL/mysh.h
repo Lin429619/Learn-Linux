b082964536c79b81fa3299e7fc22d12b8535cd6b
lin <lin.04290619@gmail.com>
2024-03-19 22:18:22 +0800

    shell 2.0

--- MY_SHELL/mysh.h
+++ MY_SHELL/mysh.h
@@ -9,13 +9,14 @@
 #include <ctype.h>
 #include <dirent.h>
 
-#define MAX_CMD 50
+#define MAX_CMD 64
 #define BUFFSIZE 255
 #define MAX_CMD_LEN 100
 #define MYSH_TOK_DELIM " \t\r\n"
 
 char * mysh_read_line();
 char ** mysh_split_line();
+void parse(char *);
 int mysh_execute(char **); 
 int mysh_launch(char **);
 int mysh_builtin_nums();
@@ -27,5 +28,13 @@ int cmd_OutPut(char *);
 int cmd_InPut(char *);
 int cmd_ReOutPut(char *);
 int cmd_Pipe(char *);
-int cmd_InBackground(char *buf);
+int cmd_InBackground(char *);
 
+char sh_path[BUFFSIZE];
+char **command;
+char *buf; //参数数组
+char backupbuf[BUFFSIZE];  //备份参数数组
+char history[MAX_CMD][BUFFSIZE];
+int cmd_num = 0;
+int argc;
+char argv[MAX_CMD][BUFFSIZE];
