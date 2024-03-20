3cd834cd111488464440fd78ce88814a48f2929e
lin <lin.04290619@gmail.com>
2024-03-20 22:17:46 +0800

    failed debug

--- MY_SHELL/mysh.h
+++ MY_SHELL/mysh.h
@@ -31,10 +31,12 @@ int cmd_Pipe(char *);
 int cmd_InBackground(char *);
 
 char sh_path[BUFFSIZE];
-char **command;
+char **command = NULL;
 char *buf; //参数数组
 char backupbuf[BUFFSIZE];  //备份参数数组
 char history[MAX_CMD][BUFFSIZE];
 int cmd_num = 0;
 int argc;
-char argv[MAX_CMD][BUFFSIZE];
+char *argv[BUFFSIZE];
+char cdform[100][BUFFSIZE]; //cd -
+int cd_num = 0;
