3750afea0f34330d5984e120ba7fecbe3aeed54e
lin <lin.04290619@gmail.com>
2024-06-05 21:39:30 +0800

    client

---
+++ SOCKET/client.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <cstring>
+
+using namespace std;
+
+class TcpSocket
+{
+public:
+    TcpSocket();
+    TcpSocket(int socket);
+    ~TcpSocket();
+    int connectToHost(string ip, unsigned short port);
+    int sendMsg(string msg);
+    string recvMsg();
+
+private:
+    int readn(char* buf, int size);
+    int writen(const char* msg, int size);
+
+private:
+    int m_fd;  // 通信的套接字
+};
+
+TcpSocket::TcpSocket()
+{
+    m_fd = socket(AF_INET, SOCK_STREAM, 0);
+}
+
+TcpSocket::TcpSocket(int socket)
+{
+    m_fd = socket;
+}
+
+TcpSocket::~TcpSocket()
+{
+    if (m_fd > 0)
+    {
+        close(m_fd);
+    }
+}
+
+int TcpSocket::connectToHost(string ip, unsigned short port)
+{
+    struct sockaddr_in saddr;
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &saddr.sin_addr.s_addr);
+    int ret = connect(m_fd, (struct sockaddr*)&saddr, sizeof(saddr));
+    if (ret == -1)
+    {
+        perror("connect");
+        return -1;
+    }
+    cout << "成功和服务器建立连接..." << endl;
+    return ret;
+}
+
+int TcpSocket::sendMsg(string msg)
+{
+    char* data = new char[msg.size() + 4];
+    int bigLen = htonl(msg.size());
+    memcpy(data, &bigLen, 4);
+    memcpy(data + 4, msg.c_str(), msg.size());
+    int ret = writen(data, msg.size() + 4);
+    delete[] data;
+    return ret;
+}
+
+string TcpSocket::recvMsg()
+{
+    int len = 0;
+    readn((char*)&len, 4);
+    len = ntohl(len);
+    cout << "数据块大小: " << len << endl;
+
+    char* buf = new char[len + 1];
+    int ret = readn(buf, len);
+    if (ret != len)
+    {
+        delete[] buf;
+        return string();
+    }
+    buf[len] = '\0';
+    string retStr(buf);
+    delete[] buf;
+
+    return retStr;
+}
+
+int TcpSocket::readn(char* buf, int size)
+{
+    int nread = 0;
+    int left = size;
+    char* p = buf;
+
+    while (left > 0)
+    {
+        if ((nread = read(m_fd, p, left)) > 0)
+        {
+            p += nread;
+            left -= nread;
+        }
+        else if (nread == -1)
+        {
+            return -1;
+        }
+    }
+    return size;
+}
+
+int TcpSocket::writen(const char* msg, int size)
+{
+    int left = size;
+    int nwrite = 0;
+    const char* p = msg;
+
+    while (left > 0)
+    {
+        if ((nwrite = write(m_fd, p, left)) > 0)
+        {
+            p += nwrite;
+            left -= nwrite;
+        }
+        else if (nwrite == -1)
+        {
+            return -1;
+        }
+    }
+    return size;
+}
+
+int main()
+{
+    TcpSocket tcp;
+
+    int ret = tcp.connectToHost("192.168.237.131", 10000);
+    if (ret == -1)
+    {
+        return -1;
+    }
+
+    int fd1 = open("english.txt", O_RDONLY);
+    int length = 0;
+    char tmp[100];
+    memset(tmp, 0, sizeof(tmp));
+    while ((length = read(fd1, tmp, sizeof(tmp))) > 0)
+    {
+        tcp.sendMsg(string(tmp, length));
+        cout << "send Msg: " << endl;
+        cout << tmp << endl << endl << endl;
+        memset(tmp, 0, sizeof(tmp));
+        usleep(300);
+    }
+
+    close(fd1);
+    sleep(10);
+
+    return 0;
+}
